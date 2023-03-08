c6feb7db0c42220fe477df5e46a08771b0365dfb
Stephen Dunning <madguy69@gmail.com>
2023-03-07 22:41:26 -0300

    Testing diffrent output command

--- uri1047.cpp
+++ uri1047.cpp
@@ -9,23 +9,24 @@ int main()
  
     cin>>salin;
  
-    if(salin>=0 && salin<=400.00)
+    
+	if(salin>=0 && salin<=400.00)
     {
         val=0.15;
     }
-    else if(salin>=400.01 && salin<=800.00)
+    	else if(salin>=400.01 && salin<=800.00)
     {
         val=0.12;
     }
-    else if(salin>=800.01 && salin<=1200.00)
+    	else if(salin>=800.01 && salin<=1200.00)
     {
         val=0.1;
     }
-    else if(salin>=1200.01 && salin<=2000.00)
+    	else if(salin>=1200.01 && salin<=2000.00)
     {
         val=0.07;
     }
-    else
+    	else
     {
         val=0.04;
     }

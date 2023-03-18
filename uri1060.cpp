c1c63c17761c410c2dbe2e978bfb3890d5edff76
Stephen Dunning <madguy69@gmail.com>
2023-03-17 23:58:09 -0300

    1060 Event Time problem

---
+++ uri1060.cpp
@@ -0,0 +1,19 @@
+Beecrowd C++ Beginner problem 1060 Even Numbers 
+
+
+
+#include<stdio.h>
+ 
+int main()
+{
+    float x;
+    int i,sum=0;
+    for(i=1;i<=6;i++){
+        scanf("%f",&x);
+        if(x>0){
+        sum=sum+1;
+        }
+    }
+    printf("%d valores positivos\n",sum);
+    return 0;//end line
+}

b962b3da96602541b27db67cf02db44326b4756d
Stephen Dunning <madguy69@gmail.com>
2023-04-07 21:28:52 -0300

    uri 1067 odd number c++

---
+++ uri1067.cpp
@@ -0,0 +1,18 @@
+Beecrowd 1067 Odd NUmber solution c++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,i;
+    cin>>n;
+    for(i=1;i<=n;i++){
+        if(i%2==1){
+            cout<<i<<endl;
+        }
+
+    }
+    return 0;
+}
+ 

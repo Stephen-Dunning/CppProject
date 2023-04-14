fa20c9831080ccb4c72ce0bff6ae2fd9af651716
Stephen Dunning <madguy69@gmail.com>
2023-04-13 22:29:19 -0300

    Only printing every other even to the user

---
+++ uri1072.cpp
@@ -0,0 +1,25 @@
+1072 C++ INterval of two -
+So ícking the evens from a group of numbers and only 
+printing every other one to the user....
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x, a;
+    
+    int in = 0;
+    int out = 0;
+    
+    cin >> x;
+    for(int i = 0; i < x; i++){
+            cin >> a;
+            if(a >= 10 && a <= 20) in++;
+            else out++;
+    }
+    cout << in << " in\n";
+    cout << out << " out\n";
+    
+    return 0;
+}

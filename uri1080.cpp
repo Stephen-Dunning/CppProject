36c0e9f8090183b72221894bf1202ffb76f70e51
Stephen Dunning <madguy69@gmail.com>
2023-05-01 22:25:56 -0300

    uri1080 fascinating while loop with multi-line output

---
+++ uri1080.cpp
@@ -0,0 +1,23 @@
+C++ solution o uri 1080 'HIghest and Position'
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    int aux = 0;
+    int pos = 0;
+    
+    for(int i = 0; i < 100; i++){
+            cin >> n;
+            if(n > aux){
+                 aux = n;
+                 pos = i;
+            }
+    }
+    
+    cout << aux << "\n" << pos+1 << "\n";
+    
+    return 0;
+}

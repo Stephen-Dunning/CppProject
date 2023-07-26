6fd5d3f360d20f4d0b6e3f54199a6810ae77c81d
Stephen Dunning <madguy69@gmail.com>
2023-07-25 22:14:27 -0300

    uri beginner 1096 Sequence IJ 2

---
+++ uri1096.cpp
@@ -0,0 +1,16 @@
+Uri 1096 Solution for 'Sequence IJ 2'
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    for(int i = 1; i < 10; i+=2){
+            for(int j = 7; j > 4; j--){
+                    cout << "I=" << i << " " << "J=" << j << "\n";
+            }
+    }
+    return 0;
+}

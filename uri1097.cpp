54b3c83836d9000c122f907a9e6a041d658075c8
Stephen Dunning <madguy69@gmail.com>
2023-07-27 23:45:24 -0300

    uri1097 Sequence IJ 3

---
+++ uri1097.cpp
@@ -0,0 +1,17 @@
+uri 1097 solution 'Sequence IJ 3'
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int j = 7;
+    for(int i = 1; i <= 9; i++){
+            cout << "I=" << i << " " << "J=" << j << "\n";
+            cout << "I=" << i << " " << "J=" << j-1 << "\n";
+            cout << "I=" << i << " " << "J=" << j-2 << "\n";
+            i++;
+            j+=2;
+    }
+    return 0;
+}

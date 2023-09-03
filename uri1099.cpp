f806af8b6b18a72d687a5e5199c80fb1408370d3
Stephen Dunning <madguy69@gmail.com>
2023-09-02 23:10:15 -0300

    uri1099 Sum of Consec Odd Numbers II

---
+++ uri1099.cpp
@@ -0,0 +1,37 @@
+Uri or Beecrowd Problem C++ Beginner 1099
+
+Sum of Consecutive Odd Numbers II
+________________________________-________
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int n;
+    int x, y, aux;
+    int soma;
+    
+    cin >> n;
+    
+    while(true){
+                if(n == 0) break;
+                cin >> x;
+                cin >> y;
+                
+                if(x > y){
+                     aux = x;
+                     x = y;
+                     y = aux;
+                }
+                soma = 0;
+                for(int i = x+1; i < y; i++){
+                       if(i%2 != 0) soma += i;
+                }
+                cout << soma << endl;
+                n--;
+    }
+    return 0;
+}

5a13c521fd30052c099c34618e803e67fd8b4c78
Stephen Dunning <madguy69@gmail.com>
2023-09-20 22:27:06 -0300

    uri 1101 list of numbers and sum C++

---
+++ uri1101.cpp
@@ -0,0 +1,34 @@
+Solution in C++ for problem 1101 'Sequence of Numbers and sum'
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int n;
+    int x, y, aux;
+    int soma;
+    
+    while(true){
+                
+                cin >> x;
+                cin >> y;
+                if(x <= 0) break;
+                if(y <= 0) break;
+                
+                if(x > y){
+                     aux = x;
+                     x = y;
+                     y = aux;
+                }
+                soma = 0;
+                for(int i = x; i <= y; i++){
+                        cout << i << " ";
+                       soma += i;
+                }
+                cout << "Sum=" << soma << endl;
+    }
+    return 0;
+}
